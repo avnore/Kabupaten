a477ac41084c13d7626c76d6dc7ac2b1c9bb0fbb
Kevin Halim <106305527+avnore@users.noreply.github.com>
2024-11-15 10:45:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -16,8 +16,8 @@ def main():
     st.title(APP_TITLE)
 
     #Import Data
-    #gdf = gpd.read_file('https://raw.githubusercontent.com/avnore/Kabupaten/blob/main/Data/kabupaten_final.shp')
-    LT = pd.read_excel('https://raw.githubusercontent.com/avnore/Kabupaten/blob/main/CB_FB_LT.xlsx')
+    #gdf = gpd.read_file('https://raw.githubusercontent.com/avnore/Kabupaten/main/Data/kabupaten_final.shp')
+    LT = pd.read_excel('https://raw.githubusercontent.com/avnore/Kabupaten/main/CB_FB_LT.xlsx')
 
     #Data Cleaning
     gdf = gdf.rename(columns={'Toko Podes': 'Podes'})
